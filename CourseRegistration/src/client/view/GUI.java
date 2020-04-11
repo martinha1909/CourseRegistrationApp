a1e99c2609b76e102e3c9ec9738a8acfb0551749
duan-le <56768212+duan-le@users.noreply.github.com>
2020-04-11 16:26:47 -0600

    Update GUI.java

--- CourseRegistration/src/client/view/GUI.java
+++ CourseRegistration/src/client/view/GUI.java
@@ -2,16 +2,12 @@ import java.awt.*;
 import javax.swing.*;
 
 public class GUI
-{
-    JFrame frame;
-    
-    public GUI(String name)
-    {
-        frame = new JFrame(name);
-    }
+{    
+    public GUI() {}
 
-    public void displayMenu()
+    public void displayMenu(String name)
     {
+        JFrame frame = new JFrame(name);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         JLabel label = new JLabel("Please select an option:");
@@ -21,8 +17,8 @@ public class GUI
         JButton b4 = new JButton("View all courses in catalogue.");
         JButton b5 = new JButton("View all courses taken by student.");
 
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridBagLayout());
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridwidth = GridBagConstraints.REMAINDER;
@@ -32,29 +28,29 @@ public class GUI
         //label.setHorizontalAlignment(JLabel.CENTER); //This centers the label
         gbc.gridx = 0;
         gbc.gridy = 0;
-        buttonPanel.add(label, gbc);
+        panel.add(label, gbc);
         
         gbc.gridx = 0;
         gbc.gridy = 1;
-        buttonPanel.add(b1, gbc);
+        panel.add(b1, gbc);
        
         gbc.gridx = 0;
         gbc.gridy = 2;
-        buttonPanel.add(b2, gbc);
+        panel.add(b2, gbc);
         
         gbc.gridx = 0;
         gbc.gridy = 3;
-        buttonPanel.add(b3, gbc);
+        panel.add(b3, gbc);
         
         gbc.gridx = 0;
         gbc.gridy = 4;
-        buttonPanel.add(b4, gbc);
+        panel.add(b4, gbc);
         
         gbc.gridx = 0;
         gbc.gridy = 5;
-        buttonPanel.add(b5, gbc);
+        panel.add(b5, gbc);
         
-        frame.add(buttonPanel, BorderLayout.CENTER);
+        frame.add(panel, BorderLayout.CENTER);
         frame.setSize(400, 300);
         frame.setResizable(false); 
         frame.setVisible(true);
