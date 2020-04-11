47dd681da2cd6ed55bc001b6ae956e5fdcd9a923
duan-le <56768212+duan-le@users.noreply.github.com>
2020-04-11 13:41:33 -0600

    Added GUI.java

---
+++ CourseRegistration/src/client/view/GUI.java
@@ -0,0 +1,41 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class GUI
+{
+    JFrame frame;
+    JButton b1;
+    JButton b2;
+    JButton b3;
+    JButton b4;
+    JButton b5;
+
+    public GUI()
+    {
+        frame = new JFrame("Course Registration");
+        b1 = new JButton("Search catalogue courses.");
+        b2 = new JButton("Add course to student courses.");
+        b3 = new JButton("Remove course from student courses.");
+        b4 = new JButton("View all courses in catalogue.");
+        b5 = new JButton("View all courses taken by student.");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        
+        frame.add(b1, gbc);
+        frame.add(b2, gbc);
+        frame.add(b3, gbc);
+        frame.add(b4, gbc);
+        frame.add(b5, gbc);
+        frame.setSize(400, 300);
+        frame.setVisible(true);     
+    }
+
+    public static void main(String[] args)
+    {
+        GUI myGUI = new GUI();
+    }
+}
