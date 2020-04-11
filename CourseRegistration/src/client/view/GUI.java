d5fe25c6a9f6a29cfa29a08363992347c550c138
duan-le <56768212+duan-le@users.noreply.github.com>
2020-04-11 16:21:41 -0600

    Update GUI.java

--- CourseRegistration/src/client/view/GUI.java
+++ CourseRegistration/src/client/view/GUI.java
@@ -4,34 +4,70 @@ import javax.swing.*;
 public class GUI
 {
     JFrame frame;
-    JButton b1;
-    JButton b2;
-    JButton b3;
-    JButton b4;
-    JButton b5;
-
+    
     public GUI(String name)
     {
         frame = new JFrame(name);
-        b1 = new JButton("Search catalogue courses.");
-        b2 = new JButton("Add course to student courses.");
-        b3 = new JButton("Remove course from student courses.");
-        b4 = new JButton("View all courses in catalogue.");
-        b5 = new JButton("View all courses taken by student.");
+    }
 
+    public void displayMenu()
+    {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridBagLayout());
+        
+        JLabel label = new JLabel("Please select an option:");
+        JButton b1 = new JButton("Search catalogue courses.");
+        JButton b2 = new JButton("Add course to student courses.");
+        JButton b3 = new JButton("Remove course from student courses.");
+        JButton b4 = new JButton("View all courses in catalogue.");
+        JButton b5 = new JButton("View all courses taken by student.");
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridBagLayout());
+
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridwidth = GridBagConstraints.REMAINDER;
         gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(5, 0, 5, 0);
         
-        frame.add(b1, gbc);
-        frame.add(b2, gbc);
-        frame.add(b3, gbc);
-        frame.add(b4, gbc);
-        frame.add(b5, gbc);
+        //label.setHorizontalAlignment(JLabel.CENTER); //This centers the label
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        buttonPanel.add(label, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        buttonPanel.add(b1, gbc);
+       
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        buttonPanel.add(b2, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        buttonPanel.add(b3, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        buttonPanel.add(b4, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        buttonPanel.add(b5, gbc);
+        
+        frame.add(buttonPanel, BorderLayout.CENTER);
         frame.setSize(400, 300);
-        frame.setVisible(true);     
+        frame.setResizable(false); 
+        frame.setVisible(true);
     }
 
+    /**
+     * Test code
+     * Remove when done
+     * @param args
+     */
+    // public static void main(String[] args)
+    // {
+    //     GUI myGUI = new GUI("Course Registration");
+    //     myGUI.displayMenu();
+    // }
 }
