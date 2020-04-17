fc2839fdfa7a33a449bea47a30d79d4ef6ac9434
duan-le <56768212+duan-le@users.noreply.github.com>
2020-04-17 17:11:12 -0600

    Update DBCredentials.java

--- CourseRegistration/src/server/model/DBCredentials.java
+++ CourseRegistration/src/server/model/DBCredentials.java
@@ -2,9 +2,7 @@ package server.model;
 
 public interface DBCredentials {
 	
-	//JDBC driver name and database URL
-    static final String JDBC_DRIVER = "";  
-    static final String DB_URL = "";
+    static final String DB_URL = "jdbc:mysql://localhost/mydb"; //name your schema mydb
 
     //Database credentials
     static final String USERNAME = "root";
