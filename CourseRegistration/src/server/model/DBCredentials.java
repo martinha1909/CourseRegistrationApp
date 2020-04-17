2f0a83af31aacaccf45dac75621f873be10e37a0
duan-le <56768212+duan-le@users.noreply.github.com>
2020-04-17 12:40:28 -0600

    Create DBCredentials.java

---
+++ CourseRegistration/src/server/model/DBCredentials.java
@@ -0,0 +1,11 @@
+public interface IDBCredentials {
+	
+	//JDBC driver name and database URL
+    static final String JDBC_DRIVER = "";  
+    static final String DB_URL = "";
+
+    //Database credentials
+    static final String USERNAME = "root";
+    static final String PASSWORD = "ensf409";
+
+}
