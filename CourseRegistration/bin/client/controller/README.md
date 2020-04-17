1c1e543c9ed75e72f18e125a9b6726608eb25d4f
martinha1909 <58370282+martinha1909@users.noreply.github.com>
2020-04-17 13:19:22 -0600

    Added lib to database

---
+++ CourseRegistration/bin/client/controller/README.md
@@ -0,0 +1,1 @@
+# Client's Controller Code
---
+++ CourseRegistration/src/server/model/Database.java
@@ -0,0 +1,5 @@
+package server.model;
+import java.sql.*;
+public class Database implements DBCredentials{
+	private Connection myConn = DriverManager.getConnection(//local host id goes here)
+}
